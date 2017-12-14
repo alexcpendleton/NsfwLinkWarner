d2e8dcd6226827852fb32155faa139d19563c5d7
Alex Pendleton <alexcpendleton@gmail.com>
2017-12-13 21:22:00 -0800

    General improvements to Submit and general UI
    * Adds Loading text and proper cursor
    * Actually fixes the cursor :) (fixes #19)
    * Adds a loading spinner when submitting link (fixes #17)
    * Removes some block shadows on hover

--- web-react/src/Submit.js
+++ web-react/src/Submit.js
@@ -18,50 +18,18 @@ class Submit extends Component {
     this.handleCopyClick = this.handleCopyClick.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({urlToMask: event.target.value});
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    this.handleValidation()
-      .then(this.create, this.displayProblem);
-  }
-  handleValidation() {
-    return new Promise((resolve, reject)=>{
-      resolve();
-    });
-  }
-  create() {
-    this.clearError();
-    const urlToMask = this.state.urlToMask;
-    return this.props.api
-      .create(urlToMask)
-      .then(this.displayCreated, this.displayProblem)
-  }
-  displayCreated(data) {
-    this.clearError();
-    this.setState({justCreated:{safeUri:data.safeUri}});
-    this.focusSafeUriInput();
-  }
-  focusSafeUriInput() {
-    if(this.safeUriInput) {
-      this.safeUriInput.focus();
-      this.safeUriInput.select();
-    }
-  }
-  displayProblem(problem) {
-    this.setState({error:problem})
-  }
-  clearError() {
-    this.setState({error:false})
-  }
   render() {
+    const isLoading = this.state.loading;
+    const containerClass = isLoading ? "loading":"";
     return (
-      <div>
+      <div className={containerClass}>
         <form className="submit-form" onSubmit={this.handleSubmit}>
           <div className="row">
-            <input type="text" value={this.state.urlToMask} onChange={this.handleChange} placeholder="Your original URL here" className="url original-url col-9 col" />  
-            <input type="submit" value="Submit" className="submit-button col-3 col" />
+            <input type="text" value={this.state.urlToMask} onChange={this.handleChange} placeholder="Your original URL here" className="url original-url col-9 col hoverlike-box-shadow" />
+            <button type="submit" className="paper-btn pseudo-submit-button col-3 col hoverlike-box-shadow">
+              <div className="normal-text">Submit</div>
+              <div className="spinner">&nbsp;</div>
+            </button>
           </div>
         </form>
         <form className="submit-form">
@@ -78,17 +46,69 @@ class Submit extends Component {
     return (
       <div id="justCreated" className="row">
         <input type="text" value={safeUri} readOnly className="url col-8 col" id="safe-uri" ref={(i)=>this.safeUriInput = i}/>
-        <button onClick={this.handleCopyClick} className="copy-button pseudo-submit-button center col-2">{this.state.copyButtonText}</button>
-        <a href={safeUri} className="paper-btn pseudo-submit-button center col-2 col">Go see it</a>
+        <button onClick={this.handleCopyClick} className="copy-button paper-btn pseudo-submit-button center col-2 hoverlike-box-shadow">{this.state.copyButtonText}</button>
+        <a href={safeUri} className="paper-btn pseudo-submit-button center col-2 col hoverlike-box-shadow">Go see it</a>
       </div>
     );
   }
   renderError() {
+    const message = this.state.error.friendlyMessage ||
+       "Sorry, there was a problem. Try again! :(";
     return (
       <div className="row">
-        <div className="col sm-12 border border-danger background-danger text-danger">Sorry, there was a problem. Try again! :(</div>
+        <div className="col sm-12 border border-danger background-danger text-danger">{message}</div>
       </div>
     )
+  }
+  handleChange(event) {
+    this.setState({urlToMask: event.target.value});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.handleValidation()
+      .then(this.create, this.displayProblem);
+  }
+  handleLoading() {
+    this.setState({
+      loading: true
+    });
+  }
+  handleLoadingFinished() {
+    this.setState({
+      loading: false
+    });
+  }
+  handleValidation() {
+    return new Promise((resolve, reject)=>{
+      resolve();
+    });
+  }
+  create() {
+    this.handleLoading();
+    this.clearError();
+    const urlToMask = this.state.urlToMask;
+    return this.props.api
+      .create(urlToMask)
+      .then(this.displayCreated, this.displayProblem)
+  }
+  displayCreated(data) {
+    this.handleLoadingFinished();
+    this.clearError();
+    this.setState({justCreated:{safeUri:data.safeUri}});
+    this.focusSafeUriInput();
+  }
+  focusSafeUriInput() {
+    if(this.safeUriInput) {
+      this.safeUriInput.focus();
+      this.safeUriInput.select();
+    }
+  }
+  displayProblem(problem) {
+    this.handleLoadingFinished();
+    this.setState({error:problem})
+  }
+  clearError() {
+    this.setState({error:false})
   }
   handleCopyClick(event) {
     this.copySafeUriToClipboard();
